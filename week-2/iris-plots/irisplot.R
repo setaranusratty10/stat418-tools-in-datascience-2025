d97b85fa770e561da4f091a677c5967331503521
Hochan Son <ohsono.email@gmail.com>
2025-05-22 19:23:17 -0700

    Hw2 submission -Hochan Son (#90)
    * add new files
    
    * added .gitignore file
    
    * update .gitignore
    
    * checkpoint
    
    * update .gitignore file
    
    * HochanSon_HW2_submission
    
    * update some comments

--- week-2/iris-plots/irisplot.R
+++ week-2/iris-plots/irisplot.R
@@ -1,3 +1,3 @@
 iris
 colnames(iris)
-pairs(iris[1:4], main = "Edgar Anderson's Iris Data", pch = 21, bg = c("red", "green3", "blue")[unclass(iris$Species)])
+pairs(iris[1:4], main = "Edgar Anderson's Iris Data", pch = 21, bg = c("red", "purple", "blue")[unclass(iris$Species)])
